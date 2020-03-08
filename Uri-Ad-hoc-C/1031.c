58da0b8195a80a5868a44ea5e2ce908b52876570
Pedro Daniel <pedro.d.jardim@hotmail.com>
2020-03-08 15:22:02 -0300

    1031 - Crise de Energia

---
+++ Uri-Ad-hoc-C/1031.c
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+
+#define true 1
+#define false 0
+
+int josephus(int n, int k);
+
+void main ()
+{
+
+	int qtsRegioes, pulo;
+
+	while (true)
+	{
+
+		scanf("%d", &qtsRegioes);
+		if (qtsRegioes == 0)
+			break;
+
+		pulo = 1;
+		while (true)
+		{
+
+			if (josephus(qtsRegioes, pulo) != 11)
+				pulo++;
+			else
+				break;
+
+		}
+
+		printf("%d\n", pulo);
+
+	}
+
+
+}
+
+int josephus(int n, int k)
+{
+
+	int i;
+	int retorno = 0;
+
+	for (i = 1; i < n; ++i)
+		retorno = (retorno + k) % i;
+
+	return retorno;
+
+}
