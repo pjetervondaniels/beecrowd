b3387055013ea36f825aeee7f2274930ddd61f71
Pedro Daniel <pedro.d.jardim@hotmail.com>
2020-03-08 16:02:46 -0300

    Inclusão dos problemas da Maratona Road to Fortaleza V 2014

---
+++ Maratona Road to Fortaleza V 2014/uri1702.java
@@ -0,0 +1,11 @@
+package RoadToFortaleza;
+
+/*  
+ * link: https://www.urionlinejudge.com.br/judge/pt/problems/view/1700
+ * 	Maratona Road to Fortaleza V 2014
+ */
+public class uri1702 {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ Maratona Road to Fortaleza V 2014/uri1703.java
@@ -0,0 +1,12 @@
+package RoadToFortaleza;
+
+/*  
+ * link: https://www.urionlinejudge.com.br/judge/pt/problems/view/1700
+ * 	Maratona Road to Fortaleza V 2014
+ */
+
+public class uri1703 {
+	public static void main(String[] args) {
+
+	}
+}
