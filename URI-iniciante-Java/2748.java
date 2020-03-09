37221893517e1bc29b6fb78b797156950dbc69da
Pedro Daniel <pedro.d.jardim@hotmail.com>
2020-03-09 09:49:34 -0300

    2748 - Saída 2

---
+++ URI-iniciante-Java/2748.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+ * 
+*/
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		System.out.println("---------------------------------------");
+		System.out.println("|        Roberto                      |");
+		System.out.println("|                                     |");
+		System.out.println("|        5786                         |");
+		System.out.println("|                                     |");
+		System.out.println("|        UNIFEI                       |");
+		System.out.println("---------------------------------------");
+		
+		sc.close();
+	}
+
+}
