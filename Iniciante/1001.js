c4ab7773b178b7ff31bf4362541ced854ffd0129
Pedro Daniel <pedrodanieljardim@gmail.com>
2020-03-29 14:22:13 -0300

    add 1001 and 1002

---
+++ Iniciante/1001.js
@@ -0,0 +1,6 @@
+var a = prompt("entre com o valor A:");
+var b = prompt("entre com o valor B:")
+
+var soma = parseInt(a)+parseInt(b);
+
+console.log(soma);
---
+++ Iniciante/1002.js
@@ -0,0 +1,3 @@
+var area;
+var pi = 3.14;
+var raio = 
