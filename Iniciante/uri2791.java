07a6dcc494ff2588ce6b032f2897e5e346fca37c
Pedro Daniel <pedrodanieljardim@gmail.com>
2020-04-01 19:07:15 -0300

    2791 - Feijão

---
+++ Iniciante/uri2791.java
@@ -0,0 +1,28 @@
+package uri;
+
+import java.util.Scanner;
+
+public class uri2791 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		
+		if(a==1) {
+			System.out.println("1");
+		}
+		if(b==1) {
+			System.out.println("2");
+		}
+		if(c==1) {
+			System.out.println("3");
+		}
+		if(d==1) {
+			System.out.println("4");
+		}
+		
+		sc.close();
+	}
+}
