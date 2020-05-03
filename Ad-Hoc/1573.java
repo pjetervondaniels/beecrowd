9dae2bf025adf04e95988793aa418dcd58e2119b
Pedro <pedro.d.jardim@hotmail.com>
2020-05-02 23:17:00 -0300

    Fix 1573 - Fábrica de Chocolate

--- Ad-Hoc/1573.java
+++ Ad-Hoc/1573.java
@@ -1,28 +1,21 @@
-package uri1573;
-
-import java.io.IOException;
-import java.util.Locale;
 import java.util.Scanner;
 
-/*
- * 
-*/
-
-public class Main {
-	public static void main(String[] args) throws IOException {
+public class Main{
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Locale.setDefault(Locale.US);
-		int a=1,b=2,c=3,r=0;
-		
-		while(a!=0 && b!=0 && c!=0) {
+		int a,b,c;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		while(a != 0 && b != 0 && c != 0) {
+			int volumnP = a*b*c;
+			int arestCube = (int) Math.pow(volumnP,1.0 / 3.0);
+			System.out.println(arestCube);
 			a = sc.nextInt();
 			b = sc.nextInt();
 			c = sc.nextInt();
-			r = (2*a*b)+(2*a*c)+(2*b*c);
-			r = (int) Math.sqrt((r/6));
-			System.out.println(r);
 		}
+		
 		sc.close();
 	}
-
 }
