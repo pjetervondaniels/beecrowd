d7adbb8b953ef08f6363f083a240a3500000ef94
Pedro Daniel Jardim <pedro.d.jardim@hotmail.com>
2020-05-04 09:27:20 -0300

    1050 - DDD

---
+++ Beginner/1050.py
@@ -0,0 +1,20 @@
+ddd = int(input())
+
+if ddd == 61:
+    print("Brasilia")
+elif ddd == 71:
+    print("Salvador")
+elif ddd == 11:
+    print("São Paulo")
+elif ddd == 22:
+    print("Rio de Janeiro")
+elif ddd == 32:
+    print("Juiz de Fora")
+elif ddd == 19:
+    print("Campinas")
+elif ddd == 27:
+    print("Vitoria")
+elif ddd == 31:
+    print("Belo Horizonte")
+elif ddd != 61 and ddd != 71 and ddd != 11 and ddd != 21 and ddd != 32 and ddd != 19 and ddd != 27 and ddd != 31:
+    print("DDD não cadastrado!")
