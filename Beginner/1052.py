68128fc4176496a3ea9a518b4fc042253e089956
Pedro Daniel Jardim <pedro.d.jardim@hotmail.com>
2020-05-04 09:28:39 -0300

    1052 - Month

---
+++ Beginner/1052.py
@@ -0,0 +1,28 @@
+month = int(input())
+
+if month == 1:
+    print("January")
+elif month == 2:
+    print("February")
+elif month == 3:
+    print("March")
+elif month == 4:
+    print("April")
+elif month == 5:
+    print("May")
+elif month == 6:
+    print("June")
+elif month == 7:
+    print("July")
+elif month == 8:
+    print("August")
+elif month == 9:
+    print("September")
+elif month == 10:
+    print("October")
+elif month == 11:
+    print("November")
+elif month == 12:
+    print("December")
+
+
