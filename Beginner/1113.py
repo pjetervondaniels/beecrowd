28bba8ece985d722d0f15a34e86f87a4bf05368e
Pedro Daniel Jardim <pedro.d.jardim@hotmail.com>
2020-05-06 05:53:44 -0300

    1113 - Crescente e Decrescente

---
+++ Beginner/1113.py
@@ -0,0 +1,11 @@
+x = int(input())
+y = int(input())
+
+while x != y:
+    if x > y:
+        print("Decrescente")
+    else:
+        print("Crescente")
+
+    x = int(input())
+    y = int(input())
