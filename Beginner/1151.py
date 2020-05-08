567a4093c6c9027eaca14c31ea4843109c782507
Pedro Daniel Jardim <pedro.d.jardim@hotmail.com>
2020-05-08 10:43:26 -0300

    1151 - Fibonacci Fácil

--- Beginner/1151.py
+++ Beginner/1151.py
@@ -1,23 +1,12 @@
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n - 2)
+
 n = -1
 
 while n < 0  or  n > 46 :
     n = int(input())
 
-i = 0
-count = 0
-ant = 0
+print('{}'.format(fibonacci(n)))
 
-while count != n:
-    print(i)
-    if i == 0:
-        i += 1
-    if i == 1 and count == 1:
-        i += 1
-    if i == 1 and count == 2:
-        ant = i
-        i += i
-    if count >= 3:
-        i += ant
-        ant = i
-
-    count += 1
